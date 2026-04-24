559cb4ad774d13e032add62d4527721796012402
Kacper Figura <figurakacper9@gmail.com>
2026-04-25 00:06:28 +0200

    Add player profile
    Add Player profile with const elements
    
    Add various character types player

---
+++ src/game/player/CharacterBodyType.java
@@ -0,0 +1,20 @@
+package game.player;
+
+public enum CharacterBodyType {
+    DEAFULTBODY(180,75),
+    KARKBODY(195,110),
+    DILERBODY(165,60),
+    KONFIDENTBODY(170,70),
+    GNIAZDOWYBODY(180,85);
+
+    private final int characterBodyHeight;
+    private final int characterBodyWeight;
+
+    CharacterBodyType(int characterBodyHeight, int characterBodyWeight) {
+        this.characterBodyHeight = characterBodyHeight;
+        this.characterBodyWeight = characterBodyWeight;
+    }
+    public int getCharacterBodyHeight() {   return characterBodyHeight; }
+    public int getCharacterBodyWeight() {   return characterBodyWeight; }
+
+}
---
+++ src/game/player/PlayerProfile.java
@@ -0,0 +1,22 @@
+package game.player;
+
+public class PlayerProfile {
+
+    private final String playerNickname; // zakładam że w grze nie bedzie możliwosci zmiany kryptonimy
+    private final int playerAge; // zakładam ze w grze nie bedzie mozliwosci zmiany wieku (być może potem dodamy zmiane wieku pomiedzy levelami)
+    private final int playerHeight;
+    private int playerWeight; // waga++ -> siła++
+
+    public PlayerProfile(String playerNickname, int playerAge) {
+        if (playerAge < 0)
+        {
+            throw new IllegalArgumentException("Age cannot be negative");
+        }
+        this.playerNickname = playerNickname;
+        this.playerAge = playerAge;
+        this.playerHeight = CharacterBodyType.DEAFULTBODY.getCharacterBodyHeight();
+        this.playerWeight = CharacterBodyType.DEAFULTBODY.getCharacterBodyWeight();
+
+    }
+
+}
