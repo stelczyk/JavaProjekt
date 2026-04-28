0f379be237c83760d8b7e8708362351bdc7644cf
Kacper Figura <figurakacper9@gmail.com>
2026-04-28 20:28:52 +0200

    Supplementing interfaces with all attributes for individual filds
    Complementing player-attributes
    
    Complementing shop-attributes

--- src/game/player/attributes/PlayerAttributes.java
+++ src/game/player/attributes/PlayerAttributes.java
@@ -2,4 +2,14 @@ package game.player.attributes;
 
 public interface PlayerAttributes {
 
+    int getStrength(); // sila
+    int getDefense(); // samoobrona
+    int getAccuracy(); // celność
+    int getStamina(); // kondycja
+    int getBrave(); // odwaga
+    int getSpeed(); // szybkość
+    int getCunning(); // cwaniactwo
+    int getPathology();
+    int getValor();
+    int getConnections();
 }
